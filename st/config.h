5cf624456322f5508a309c830fd5c953422e4c8a
Shimanoff <shiman93@mail.ru>
2020-03-06 13:45:25 +0300

    grey theme added without Xresources support

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -85,30 +85,35 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
+	[0] = "#505050", /* black   */
+	[1] = "#d77f7a", /* red     */
+	[2] = "#c6ca67", /* green   */
+	[3] = "#f6d16a", /* yellow  */
+	[4] = "#4c5359", /* blue    */
+	[5] = "#99828e", /* magenta */
+	[6] = "#a2ccca", /* cyan    */
+	[7] = "#5e5e5e", /* white   */
+	
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "#5c5c5c", /* black   */
+	[9]  = "#d77f7a", /* red     */
+	[10] = "#b5bc6d", /* green   */
+	[11] = "#877f69", /* yellow  */
+	[12] = "#89abba", /* blue    */
+	[13] = "#cca8bb", /* magenta */
+	[14] = "#a2ccca", /* cyan    */
+	[15] = "#707070", /* white   */
+	
+
+    /* special colors */
+    [256] = "#272d32", /* background */
+    [257] = "#b8b3a3", /* foreground */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	/*"#cccccc",
+	"#555555",*/
 };
 
 
@@ -116,10 +121,13 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor
