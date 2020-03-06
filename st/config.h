a71d9da216025cf0130c706b488174cf086d9f7a
Shimanoff <shiman93@mail.ru>
2020-03-06 13:20:01 +0300

    for now I don't need Xresource support

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -151,44 +151,6 @@ static unsigned int mousebg = 0;
 static unsigned int defaultattr = 11;
 
 /*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-    { "font",         STRING,  &font },
-    { "color0",       STRING,  &colorname[0] },
-    { "color1",       STRING,  &colorname[1] },
-    { "color2",       STRING,  &colorname[2] },
-    { "color3",       STRING,  &colorname[3] },
-    { "color4",       STRING,  &colorname[4] },
-    { "color5",       STRING,  &colorname[5] },
-    { "color6",       STRING,  &colorname[6] },
-    { "color7",       STRING,  &colorname[7] },
-    { "color8",       STRING,  &colorname[8] },
-    { "color9",       STRING,  &colorname[9] },
-    { "color10",      STRING,  &colorname[10] },
-    { "color11",      STRING,  &colorname[11] },
-    { "color12",      STRING,  &colorname[12] },
-    { "color13",      STRING,  &colorname[13] },
-    { "color14",      STRING,  &colorname[14] },
-    { "color15",      STRING,  &colorname[15] },
-    { "background",   STRING,  &colorname[256] },
-    { "foreground",   STRING,  &colorname[257] },
-    { "cursorColor",  STRING,  &colorname[258] },
-    { "termname",     STRING,  &termname },
-    { "shell",        STRING,  &shell },
-    { "xfps",         INTEGER, &xfps },
-    { "actionfps",    INTEGER, &actionfps },
-    { "blinktimeout", INTEGER, &blinktimeout },
-    { "bellvolume",   INTEGER, &bellvolume },
-    { "tabspaces",    INTEGER, &tabspaces },
-    { "borderpx",     INTEGER, &borderpx },
-    { "cwscale",      FLOAT,   &cwscale },
-    { "chscale",      FLOAT,   &chscale },
-};
-
-
-
-/*
  * Force mouse select/shortcuts while mask is active (when MODE_MOUSE is set).
  * Note that if you want to use ShiftMask with selmasks, set this to an other
  * modifier, set to 0 to not use it.
@@ -201,8 +163,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-    { ShiftMask,            Button4, kscrollup,      {.i = 1} },
-    { ShiftMask,            Button5, kscrolldown,    {.i = 1} },
+	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
+	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
