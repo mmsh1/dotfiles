79016179cc52b5f42ac3c9432add0f0aa7e76917
mm-sh <shiman93@mail.ru>
2020-06-19 15:20:02 +0300

    another dual display stuff

--- magenta-pink/polybar/launch.sh
+++ magenta-pink/polybar/launch.sh
@@ -8,6 +8,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # launch polybar, using default config location 
 # ~/.config/polybar/config
-polybar mybar &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
 echo "Polybar launched..."
