ffe4ffb5f4261a608b3dff93287ed3fab6f3ed58
Михаил Шиманов <mihail.shimanov@bergen.tech>
2024-05-26 19:55:53 +0300

    st terminal patched in order to draw wide glyphs properly

--- st/config.def.h
+++ st/config.def.h
@@ -1,8 +1,11 @@
-/* for nostalgic atmospere. font zip is here https://int10h.org/oldschool-pc-fonts/fontlist/ */
-/* static char *font = "MxPlus AST PremiumExec:pixelsize=30:antialias=true:autohint=false"; */
-/* static char *font = "JetBrainsMono-Regular:pixelsize=20:antialias=true:autohint=true"; */
-static char *font = "InputMono-Regular:pixelsize=20:antialias=true:autohint=true";
-/*static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";*/
+/* See LICENSE file for copyright and license details. */
+
+/*
+ * appearance
+ *
+ * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
+ */
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -93,34 +96,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#1e1e1e",
-	[1] = "#be0f17",
-    [2] = "#868715",
-    [3] = "#cc881a",
-    [4] = "#377375",
-    [5] = "#a04b73",
-    [6] = "#578e57",
-    [7] = "#978771",
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
 	/* 8 bright colors */
-	[8] = "#7f7061",
-	[9] = "#f73028",
-	[10] = "#aab01e",
-	[11] = "#f7b125",
-	[12] = "#719586",
-	[13] = "#c77089",
-	[14] = "#7db669",
-	[15] = "#e6d4a3",
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
 	[255] = 0,
 
-	/* special colors */
-    [256] = "#1e1e1e", /* background */
-    [257] = "#e6d4a3" /* foreground */
-
 	/* more colors can be added after 255 to use with DefaultXX */
-	/* "#cccccc", */
-	/* "#555555", */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 };
 
 
@@ -128,14 +129,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-
-unsigned int defaultcs = 257;
-unsigned int defaultrcs = 257;
-
-/*static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;*/
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
@@ -177,14 +174,12 @@ static uint forcemousemod = ShiftMask;
  * Internal mouse shortcuts.
  * Beware that overloading Button1 will disable the selection.
  */
-	//{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	//{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ ShiftMask,            Button4, kscrollup,      {.i = 1} },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 1} },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
+	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
